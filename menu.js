3dc1241fb640d8558456cd2b24fd8427d80bc848
saatang <satang@workbythai.com>
2019-01-09 13:27:34 +0700

    update menu

--- menu.js
+++ menu.js
@@ -2,15 +2,23 @@
 
 var menu = [
 	
-	{
-		name : 'แดชบอร์ด',
-		link : 'index.html'
-	},
+
 	{
         name: 'รับรถเข้า',
         link: 'receivecar.html',
         sub: null
+    },
+    {
+    	name: 'ข้อตกลงมัดจำรถ used',
+    	link: 'deposit.html',
+    	sub: null
+    },
+    {
+    	name: 'สัญญามัดจำ',
+    	link: 'deposit_contract.html',
+    	sub: null
     }
+
 ];
 var data = "";
 
